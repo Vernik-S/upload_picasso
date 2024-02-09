ac82763ca566e7eb6034c39912a52b228a375a94
Nikita Vershigora <nikita.vershigora@yandex.ru>
2024-02-09 21:50:10 +0100

    File model created

---
+++ app/upload_file/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.db.models import FileField, Model, DateTimeField, BooleanField
+
+
+# Create your models here.
+class File(Model):
+    file = FileField(blank=False)
+    created_at = DateTimeField(auto_now_add=True, verbose_name="When was this file uploaded")
+    processed = BooleanField(verbose_name='file processed status', default=False)
