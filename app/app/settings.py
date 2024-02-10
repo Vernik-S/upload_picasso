0ddfc0b8b94cd21fd2b72e4c780511065e130dc9
Nikita Vershigora <nikita.vershigora@yandex.ru>
2024-02-10 18:12:02 +0100

    Upload to media folder

--- app/app/settings.py
+++ app/app/settings.py
@@ -9,12 +9,14 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Here
+MEDIA_URL = '/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -38,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'rest_framework'
+    'rest_framework',
 
     'upload_file',
 ]
--- app/upload_file/models.py
+++ app/upload_file/models.py
@@ -4,6 +4,6 @@ from django.db.models import FileField, Model, DateTimeField, BooleanField
 
 # Create your models here.
 class File(Model):
-    file = FileField(blank=False)
+    file = FileField()
     created_at = DateTimeField(auto_now_add=True, verbose_name="When was this file uploaded")
     processed = BooleanField(verbose_name='file processed status', default=False)
