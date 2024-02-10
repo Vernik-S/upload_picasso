238c1dc878117ba012c5d948822216d5b1c47fe0
Nikita Vershigora <nikita.vershigora@yandex.ru>
2024-02-10 18:15:35 +0100

    field processed is read only

--- app/upload_file/serializers.py
+++ app/upload_file/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 
-from app.upload_file.models import File
+from .models import File
 
 
 class FileSerializer(serializers.ModelSerializer):
     class Meta:
         model = File
-        fields = ['id', 'file', 'created_at', 'processed']
+        fields = ['id', 'file', 'created_at',]
+        read_only_fields = ['processed']
