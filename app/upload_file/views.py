7921f4f58dc146733088aec1625e1e419f051f05
Nikita Vershigora <nikita.vershigora@yandex.ru>
2024-02-10 17:26:32 +0100

    Base viewset

--- app/upload_file/views.py
+++ app/upload_file/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet, GenericViewSet
+
+from app.upload_file.serializers import FileSerializer
+
 
 # Create your views here.
+class UploadViewSet(GenericViewSet):
+    """
+    Class for uploading files
+    """
+    serializer_class = FileSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(serializer)
+
+        
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
