aba519c7ce8420847c02685cc5cdd633ebba8f72
Nikita Vershigora <nikita.vershigora@yandex.ru>
2024-02-11 12:11:55 +0100

    task reworked

--- app/upload_file/tasks.py
+++ app/upload_file/tasks.py
@@ -14,20 +14,19 @@ from upload_file.models import File
 #@app.task
 def process_file(file_id, content_type = None):
     file = File.objects.get(id=file_id)
+    file_name = file.file.path
     if content_type in ['image/jpeg', 'image/tiff']:
-        file_name = file.file.path
-        print(file_name)
         image = Image.open(file_name)
         inverted_image = PIL.ImageOps.invert(image)
         inverted_image.save(file_name)
     elif content_type in ['text/plain']:
         new_lines = []
-        with open(file.file.path, ) as f:
+        with open(file_name, "r") as f:
             for line in f:
                 randomized_line = ''.join(choice((str.upper, str.lower))(char) for char in line)
                 print(randomized_line)
                 new_lines.append(randomized_line)
-        with open(file.file.path, "w") as f:
+        with open(file_name, "w") as f:
             f.writelines(new_lines)
 
 
