fdcba63f40956ce32261337c08ad1389ca2cdecc
Nikita Vershigora <nikita.vershigora@yandex.ru>
2024-02-25 17:31:19 +0100

    Celery testing init

--- app/tests/upload_file/test_api.py
+++ app/tests/upload_file/test_api.py
@@ -1,9 +1,20 @@
+import filecmp
+import os
+import random
+import string
+from io import BytesIO, StringIO
+from time import sleep
+
 import pytest
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 from rest_framework.test import APIClient
 from model_bakery import baker
 
 from upload_file.models import File
+from upload_file.tasks import process_file
+from django.core.files import File as Fl
+from django.contrib.staticfiles import finders
 
 
 @pytest.fixture
@@ -37,3 +48,28 @@ def test_list_files(client, files_factory):
     for i, response_file in enumerate(data):
         assert response_file['file'].split('/')[-1] == files_test[i].file
         assert response_file['processed'] is False
+
+
+@pytest.mark.celery(result_backend='redis://localhost:6379')
+@pytest.mark.django_db
+def test_processing_file(client):
+    # Arrange
+    random_string = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(100))
+    upload_file = SimpleUploadedFile('testfile.txt', random_string.encode())
+    response = client.post(f'/api/v1/upload/', {'file': upload_file}, follow=True, )
+    file_id = response.data["id"]
+    print(file_id)
+
+    # Act
+    process_file.delay(file_id, content_type='text/plain')
+
+    # Assert
+
+    sleep(2) #waiting for celery result
+
+    file_url = response.data['file']
+    file_response = client.get(file_url)
+
+    file_content = "".join([chunk.decode() for chunk in file_response])
+
+    assert random_string.encode() != file_content.encode()
